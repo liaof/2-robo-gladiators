9b661bdd283f4ed246e019fad65be161ccd3aa30
liaof <f.liao163@gmail.com>
2021-03-08 18:07:16 -0500

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,9 @@
+var playerName = window.prompt("What is your robot's name?");
+
+console.log("Our player's name is "+playerName);
+
+function fight () {
+    window.alert("The fight has begun");
+}
+
+//fight();
