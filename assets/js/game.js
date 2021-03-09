1f7c154d5a8eda4b55082900a2d1bb17a74163bb
liaof <f.liao163@gmail.com>
2021-03-08 21:53:56 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,22 +1,22 @@
+// You can also log multiple values at once like this
+//console.log(playerName, playerAttack, playerHealth);
 var playerName = window.prompt("What is your robot's name?");
+
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multiple values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    var promptFight = window.prompt("Would you like to fight or skip this battle? Enter 'fight' or 'skip' to choose.").toLowerCase();
 
     //if player chooses to fight, then fight
     // === checks for value and data type
-    if (promptFight === "fight" || promptFight==="FIGHT"){
+    if (promptFight === "fight"){
         enemyHealth = enemyHealth - playerAttack;
         console.log(
             playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
@@ -44,7 +44,7 @@ var fight = function() {
         }
     } 
     // if player choses to skip
-    else if (promptFight === "skip" || promptFight === "SKIP") {
+    else if (promptFight === "skip") {
         window.alert(playerName + " has chosen to skip the fight!");
         var confirmSkip = window.confirm ("Are you sure you'd like to quit");
 
@@ -56,7 +56,16 @@ var fight = function() {
     } 
     else {
         window.alert("You need to choose a valid option. Try again!");
+        fight();
     }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index");
+}
+
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
